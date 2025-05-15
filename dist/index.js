ec1982c12bc6ef578b9f9dac6089de250804a8ae
adop <chdn786od@gmail.com>
2025-05-15 21:21:46 +0200

    class list is done; next: students list

--- dist/index.js
+++ dist/index.js
@@ -1,76 +1,49 @@
 "use strict";
-//data
-// const DEFAULT_DATA: Data = [
-//     {
-//         class: "5H",
-//         students: [
-//             {
-//                 name: "Olexander"
-//             },
-//             {
-//                 name: "Daniil"
-//             },
-//             {
-//                 name: "Mahdi"
-//             },
-//             {
-//                 name: "Alex"
-//             }
-//         ]
-//     },
-//     {
-//         class: "6D",
-//         students: [
-//             {
-//                 name: "Ali"
-//             },
-//             {
-//                 name: "Mango"
-//             },
-//             {
-//                 name: "Oleg"
-//             },
-//             {
-//                 name: "Sasha"
-//             },
-//             {
-//                 name: "Pasha"
-//             }
-//         ]
-//     }
-// ];
 //variables
 const KEY = "class";
 //elements
-const classListElem = document.querySelector("[data-list]");
-const addButton = document.querySelector("#add");
+const classListElem = document.querySelector('[data-list]');
+const addButton = document.querySelector('#add');
 const addPopUp = document.querySelector('#addPopUp');
 const closeAddBtn = document.querySelector('#closeAddBtn');
 const addClassForm = document.querySelector('#addClassForm');
 const classNameInput = document.querySelector('#name');
+const classNameTitle = document.querySelector('.class_name_title');
 //set data from local-storage 
 const setData = () => {
-    // if (!localStorage.getItem(KEY)) {
-    //     localStorage.setItem(KEY, JSON.stringify(DEFAULT_DATA));
-    // }
     let raw = localStorage.getItem(KEY);
-    const data = raw ? JSON.parse(raw) : [];
+    const data = raw ? JSON.parse(raw) : localStorage.setItem(KEY, "[]");
     return data;
+};
+//id generator
+const idGeneration = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
 //html code generator
 const generateClassListItems = (data) => {
     return data
-        .map((e) => `<li class="class_item">
-            <h3>${e.class}</h3>
-            <span>${e.students.length}P.</span>
-        </li>`)
+        .map((e) => liItemGenerator(e))
         .join("");
 };
 //render data into HTML
 const renderData = () => {
     const data = setData();
     const liItems = generateClassListItems(data);
-    classListElem === null || classListElem === void 0 ? void 0 : classListElem.insertAdjacentHTML("beforeend", liItems);
+    classListElem === null || classListElem === void 0 ? void 0 : classListElem.insertAdjacentHTML("beforeend", liItems !== "" ? liItems : '<li style="text-align: center; font-weight: 500; font-size: 19px;">Sie haben noch keine Klassen</li>');
+};
+//li item generator
+const liItemGenerator = (e) => {
+    return `<li class="class_item">
+                <h3>${e.class}</h3>
+                <div>
+                    <button type="button" id="deleteClassButton" data-id="${e.id}">
+                        <svg>
+                            <use href="./public/symbol-defs.svg#trash" />
+                        </svg>
+                    </button>
+                    <span>${e.students.length}P.</span>
+                </div>
+            </li>`;
 };
 //toggleVisibility callback
 const toggleVisibility = (element) => {
@@ -91,25 +64,34 @@ addClassForm === null || addClassForm === void 0 ? void 0 : addClassForm.addEven
     if (!className)
         return;
     const actualData = setData();
+    const id = idGeneration(1, 1000000);
     const newClass = {
         class: className,
+        id,
         students: []
     };
     actualData.push(newClass);
     localStorage.setItem(KEY, JSON.stringify(actualData));
     const updatedData = setData();
-    const lastClass = updatedData[updatedData.length - 1];
-    if (classListElem) {
-        classListElem.insertAdjacentHTML("beforeend", `<li class="class_item">
-                <h3>${lastClass.class}</h3>
-                <span>${lastClass.students.length}P.</span>
-            </li>`);
-    }
+    classListElem.innerHTML = generateClassListItems(updatedData);
     if (classNameInput) {
         classNameInput.value = "";
     }
     ;
     addPopUp ? toggleVisibility(addPopUp) : console.log("Type of 'addPopUp' is null");
+});
+//delete class
+classListElem === null || classListElem === void 0 ? void 0 : classListElem.addEventListener("click", (event) => {
+    const target = event.target;
+    const button = target.closest("button[data-id]");
+    if (!button)
+        return;
+    const id = parseInt(button.dataset.id);
+    const actualData = setData();
+    const newData = actualData.filter(item => item.id !== id);
+    localStorage.setItem(KEY, JSON.stringify(newData));
+    classListElem.innerHTML = "";
+    renderData();
 });
 //app-start
 renderData();
--- src/index.ts
+++ src/index.ts
@@ -5,94 +5,68 @@ type Student = {
     
 type SchoolClass = {
     class: string;
+    id: number,
     students: Student[];
 };
     
 type Data = SchoolClass[];
 
-//data
-// const DEFAULT_DATA: Data = [
-//     {
-//         class: "5H",
-//         students: [
-//             {
-//                 name: "Olexander"
-//             },
-//             {
-//                 name: "Daniil"
-//             },
-//             {
-//                 name: "Mahdi"
-//             },
-//             {
-//                 name: "Alex"
-//             }
-//         ]
-//     },
-//     {
-//         class: "6D",
-//         students: [
-//             {
-//                 name: "Ali"
-//             },
-//             {
-//                 name: "Mango"
-//             },
-//             {
-//                 name: "Oleg"
-//             },
-//             {
-//                 name: "Sasha"
-//             },
-//             {
-//                 name: "Pasha"
-//             }
-//         ]
-//     }
-// ];
-
 //variables
 const KEY: string = "class";
 
 //elements
-const classListElem = document.querySelector<HTMLUListElement>("[data-list]");
-const addButton = document.querySelector<HTMLButtonElement>("#add");
+const classListElem = document.querySelector<HTMLUListElement>('[data-list]');
+const addButton = document.querySelector<HTMLButtonElement>('#add');
 const addPopUp = document.querySelector<HTMLDivElement>('#addPopUp');
 const closeAddBtn = document.querySelector<HTMLDivElement>('#closeAddBtn');
 const addClassForm = document.querySelector<HTMLFormElement>('#addClassForm');
 const classNameInput = document.querySelector<HTMLInputElement>('#name');
+const classNameTitle = document.querySelector<HTMLTitleElement>('.class_name_title');
 
 //set data from local-storage 
-const setData = (): Data => {
-    // if (!localStorage.getItem(KEY)) {
-    //     localStorage.setItem(KEY, JSON.stringify(DEFAULT_DATA));
-    // }
-    
+const setData = (): Data => {    
     let raw: string | null = localStorage.getItem(KEY);
 
-    const data: Data = raw ? JSON.parse(raw) : [];
+    const data: Data = raw ? JSON.parse(raw) : localStorage.setItem(KEY, "[]");
 
     return data;   
+};
+
+//id generator
+const idGeneration = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 //html code generator
 const generateClassListItems = (data: Data): string => {
     return data
     .map((e) => 
-        `<li class="class_item">
-            <h3>${e.class}</h3>
-            <span>${e.students.length}P.</span>
-        </li>`
+        liItemGenerator(e)
     )
     .join("");
 };
 
 //render data into HTML
-const renderData = () => {
+const renderData = (): void => {
     const data: Data = setData();
     const liItems: string = generateClassListItems(data);
 
-    classListElem?.insertAdjacentHTML("beforeend", liItems);
+    classListElem?.insertAdjacentHTML("beforeend", liItems !== "" ? liItems : '<li style="text-align: center; font-weight: 500; font-size: 19px;">Sie haben noch keine Klassen</li>');
+};
+
+//li item generator
+const liItemGenerator = (e: SchoolClass): string => {
+    return `<li class="class_item">
+                <h3>${e.class}</h3>
+                <div>
+                    <button type="button" id="deleteClassButton" data-id="${e.id}">
+                        <svg>
+                            <use href="./public/symbol-defs.svg#trash" />
+                        </svg>
+                    </button>
+                    <span>${e.students.length}P.</span>
+                </div>
+            </li>`
 };
 
 //toggleVisibility callback
@@ -121,33 +95,44 @@ addClassForm?.addEventListener("submit", (e) => {
     if (!className) return;
     
     const actualData: Data = setData();
+    const id: number = idGeneration(1, 1000000);
     const newClass: SchoolClass = {
-        class: className, 
+        class: className,
+        id, 
         students: []
-    };   
+    };
 
     actualData.push(newClass);
     localStorage.setItem(KEY, JSON.stringify(actualData));
     
-    const updatedData = setData();
-    const lastClass = updatedData[updatedData.length - 1];
+    const updatedData: Data = setData();
 
-    if (classListElem) {
-        classListElem.insertAdjacentHTML(
-            "beforeend",
-            `<li class="class_item">
-                <h3>${lastClass.class}</h3>
-                <span>${lastClass.students.length}P.</span>
-            </li>`
-        );
-    }
+    classListElem!.innerHTML = generateClassListItems(updatedData);
     
     if (classNameInput) {
         classNameInput.value = "";
     };
 
     addPopUp ? toggleVisibility(addPopUp) : console.log("Type of 'addPopUp' is null");
-})
+});
+
+//delete class
+classListElem?.addEventListener("click", (event) => {
+    const target = event.target as HTMLElement;
+
+    const button = target.closest("button[data-id]") as HTMLButtonElement;
+    if (!button) return;
+
+    const id = parseInt(button.dataset.id!);
+    const actualData: Data = setData();
+
+    const newData: Data = actualData.filter(item => item.id !== id);
+
+    localStorage.setItem(KEY, JSON.stringify(newData));
+
+    classListElem.innerHTML = "";
+    renderData();
+});
 
 //app-start
 renderData();
